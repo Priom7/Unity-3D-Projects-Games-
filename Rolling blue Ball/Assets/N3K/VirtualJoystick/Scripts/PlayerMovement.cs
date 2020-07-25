f10aad49e5c23b67311a38a5a937fa2f15ac86ce
Md. Sharif Alam <priom7197@gmail.com>
2020-07-25 21:48:07 +0600

    Unity 3D learning Basics project and Augmented Reality Project

---
+++ Rolling blue Ball/Assets/N3K/VirtualJoystick/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public VirtualJoystick joystick;
+    public float speed = 350.0f;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rigid.AddForce(joystick.InputDirection * speed*10 * Time.deltaTime);
+    }
+}
